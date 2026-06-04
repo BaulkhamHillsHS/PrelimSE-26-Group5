8ed60bf58c16572e15b910758c82a088e173b47a
sigh0 <benjamin.huang5@education.nsw.gov.au>
2026-06-04 13:45:29 +0000

    worked a bit on the login screen. Still getting the password and username screen to work

--- gui.py
+++ gui.py
@@ -4,7 +4,23 @@ import csv
 
 class MainPage(ctk.CTk):
     def __init__(self):
-        super().__init__
-        self.title("Maxeth Streaming Client")
-        self.geometry("600x400")
-        self.build_ui()
+        super().__init__()
+        self.geometry("400x300")
+        self.title("Maxeth")
+
+app = MainPage()
+app.mainloop()
+
+def login():
+    username = "test"
+    password = "test2"
+    
+    new_window = ctk.CTkTopLevel(app)
+    new_window.title("new window")
+    new_window.geometry("350x150")
+    
+    if user_entry.get() == username and user_pass.get() == password:
+        tkmb.showinfo("title="Login Sucessful", 
+        message="You have logged in")
+        ctk.CTkLavel(new_window,
+                     text="bruh").pack()
