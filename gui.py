6c1bd426527c2c783f66ad2cb0518bee5a242741
sigh0 <benjamin.huang5@education.nsw.gov.au>
2026-06-10 01:28:44 +0000

    started working on search bar

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 import os 
 import csv
 
-ctk.set_appearnce_mode("dark")
+ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
 app = ctk.CTk()
@@ -17,7 +17,7 @@ def login():
     new_window.title("new window")
     new_window.geometry("350x150")
     
-label = ctk.CTkLabel(self, text = "Login")
+label = ctk.CTkLabel(app, text = "Login")
 label.pack(pady=20)
 
 frame = ctk.CTkFrame(master=app)
@@ -29,10 +29,22 @@ label.pack(pady=12,padx=10)
 user_entry = ctk.CTkEntry(master=frame, text = "Login Here")
 user_entry.pack(pady=20)
 
-button = ctk.CTkButton(master=frame,text='Login' ,command=login)
-button.pack(pady=12,padx=10)
+loginbutton = ctk.CTkButton(master=frame,text='Login' ,command=login)
+loginbutton.pack(pady=12,padx=10)
 
-checkbox = ctk.CTKCheckBox(master=frame,text='Remember Me')
+checkbox = ctk.CTkCheckBox(master=frame,text='Remember Me')
 checkbox.pack(pady=12,padx=10)
+
+def searchbar():
+
+textbox = ctk.CTkTextbox(
+    master=app,
+    width=400,
+    height=200,
+    corner_radius=10,
+    border_width=2
+)
+textbox.pack(pady=20, padx=20, fill="both", expand=True)
+
 
 app.mainloop()
